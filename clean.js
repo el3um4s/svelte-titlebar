e10fdfe6131a57267a01828ef95cddb354573564
Samuele de Tomasi <el3um4s@gmail.com>
2021-11-05 15:27:08 +0100

    added gh-pages and @sveltejs/adapter-static

---
+++ clean.js
@@ -0,0 +1,24 @@
+import {
+    existsSync,
+    rmSync
+} from 'fs';
+
+function deleteFolderRecursive(path) {
+    console.log(`Deleting directory "${path}"...`);
+    if (existsSync(path)) {
+        rmSync(path, {
+            force: true,
+            recursive: true
+        });
+    }
+};
+
+console.log("Cleaning old files...");
+
+const paths = ["./build"];
+
+paths.forEach(path => {
+    deleteFolderRecursive(path);
+});
+
+console.log("Successfully cleaned!");
--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -18,7 +19,12 @@ const config = {
 		package: {
 			dir: 'package',
 			emitTypes: true
-		}
+		},
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		}),
 	}
 };
 
