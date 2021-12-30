9e50e71a7365e533bcbfff4e92dcf8ca50b6a889
Samuele De Tomasi <el3um4s@gmail.com>
2021-12-30 13:07:39 +0100

    update documentation

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -34,8 +34,18 @@
 		isMaximized = true;
 	}
 
-	let backgroundColor = '#1e609f';
-	let textColor = '#DBDBDB';
+	let backgroundColor = '#e5e7eb';
+	let textColor = '#1f2937';
+
+	$: if (theme === 'theme-light') {
+		backgroundColor = '#e5e7eb';
+		textColor = '#1f2937';
+	}
+
+	$: if (theme === 'theme-dark') {
+		backgroundColor = '#1f2937';
+		textColor = '#e5e7eb';
+	}
 </script>
 
 <section class="svelte-info">
@@ -47,24 +57,22 @@
 		--background-color="#f9fafb"
 		--text-color="#1f2937"
 	>
-		<span slot="about">
-			<div class="titlebar">
-				<TitleBar
-					{title}
-					{isMaximized}
-					on:clickMinimize={() => log('minimize')}
-					on:clickUnmaximize={unmaximize}
-					on:clickMaximize={maximize}
-					on:clickClose={() => log('close')}
-					class={theme}
-					--background-color={backgroundColor}
-					--text-color={textColor}
-				/>
-				<div class="log">Click: {logs}</div>
+		<span slot="demo">
+			<TitleBar
+				{title}
+				{isMaximized}
+				on:clickMinimize={() => log('minimize')}
+				on:clickUnmaximize={unmaximize}
+				on:clickMaximize={maximize}
+				on:clickClose={() => log('close')}
+				class={theme}
+				--background-color={backgroundColor}
+				--text-color={textColor}
+			/>
+			<div class="log" style="background-color:{backgroundColor}; color:{textColor};">
+				Click: {logs}
 			</div>
-
 			<div class="details">
-				<div class="title">Props</div>
 				<label>
 					<span class="label">title</span>
 					<input type="text" bind:value={title} />
@@ -73,10 +81,7 @@
 					<span class="label">isMaximized</span>
 					<input type="checkbox" bind:checked={isMaximized} />
 				</label>
-			</div>
 
-			<div class="details">
-				<div class="title">Colors</div>
 				<label>
 					<span class="label">Themes</span>
 					<select bind:value={theme} class="themes">
@@ -131,7 +136,7 @@
 	}
 
 	.log {
-		@apply w-full h-14 mt-3 p-2 bg-gray-900 text-yellow-200;
+		@apply w-full;
 	}
 
 	.titlebar {
@@ -139,14 +144,7 @@
 	}
 
 	.details {
-		@apply flex flex-col border border-gray-300 p-2 m-2;
-	}
-
-	.details .title {
-		@apply bg-gray-200 text-gray-800 font-bold;
-		margin-top: -8px;
-		margin-left: -8px;
-		margin-right: -8px;
+		@apply flex flex-col;
 	}
 
 	span.label {
